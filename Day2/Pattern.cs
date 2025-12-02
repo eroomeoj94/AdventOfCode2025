df509d0adfc1787b5b77b1a00cda3b1744ee998e
Joe Moore <joe.m@artlist.io>
2025-12-02 12:53:36 +0000

    Added comment for clarity

--- Day2/Pattern.cs
+++ Day2/Pattern.cs
@@ -13,10 +13,13 @@ public class Pattern
         var stringVal = number.ToString();
         var stringValLength = stringVal.Length;
         
+        // Loop through all possible splits
         for (int i = 1; i < stringValLength; i++)
         {
+            // Create sub strings of those splits
             var splits = Split(stringVal, i).ToList();
             
+            // If there is only one split then we can't have a pattern
             if(!(splits.Count > 1))
                 continue;
             
@@ -37,9 +40,17 @@ public class Pattern
         return false;
     }
     
+    /// <summary>
+    /// Splits a string into chunks of a given size e.g. "123456789" into "123", "456", "789" if chunk size is 3
+    /// FYI it cuts off the last chunk if it's not a full chunk.
+    /// </summary>
+    /// <param name="str">The string to split</param>
+    /// <param name="chunkSize"></param>
+    /// <returns>An array of splitted up strings</returns>
     private static IEnumerable<string> Split(string str, int chunkSize)
     {
-        return Enumerable.Range(0, str.Length / chunkSize)
+        return Enumerable
+            .Range(0, str.Length / chunkSize)
             .Select(i => str.Substring(i * chunkSize, chunkSize));
     }
     
